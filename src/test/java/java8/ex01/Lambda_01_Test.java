0f470cefd2143c17e067ea3f17bfc37ee56233cc
KevinLeNozer <kevin.lenozer@gmail.com>
2022-07-11 12:05:47 +0200

    Exo_1Fini

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -2,8 +2,11 @@ package java8.ex01;
 
 import java8.data.Data;
 import java8.data.Person;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
+import javax.sql.rowset.Predicate;
+import java.security.DigestInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +42,8 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        PersonPredicate personpre = (Person p) -> p.getAge() >= 18;
+        List<Person> result = filter(personList, personpre);
 
         assert result.size() == 83;
 
@@ -56,7 +60,8 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        PersonPredicate personFirstName = (Person p) -> p.getFirstname().equals("first_10");
+        List<Person> result = filter(personList, personFirstName);
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -73,8 +78,14 @@ public class Lambda_01_Test {
         String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
+            PersonPredicate personAgePass = new PersonPredicate() {
+                @Override
+                public boolean test(Person p) {
+                    return p.getAge() > 49 && DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex);
+                }
+            };
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, personAgePass);
 
         assert result.size() == 6;
         for (Person person : result) {
